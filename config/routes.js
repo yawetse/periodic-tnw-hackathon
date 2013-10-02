a7e932f59782894f967aa2546339882727d61c90
Yaw Joseph Etse <yaw.etse@gmail.com>
2013-10-02 16:22:32 -0400

    live query for module data

--- config/routes.js
+++ config/routes.js
@@ -51,6 +51,7 @@ exports = module.exports = function(app) {
 	app.get('/periodic/content/:contentid', user.ensureAuthenticated, content.loadContent, content.show);
 	app.post('/periodic/content/new', content.create);
 
+	app.post('/periodic/content/query', user.ensureAuthenticated, content.query)
 	app.post('/periodic/content/:contentid/update', user.ensureAuthenticated, content.loadContent, content.updateContentDocuments, content.show)
 	// **************** 
 	//     site
